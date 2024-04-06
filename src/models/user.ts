a917f22ea2907544ef3af606a7949fbbb07243bc
Garvit <garvitmaloo.21@gmail.com>
2024-04-07 02:22:05 +0530

    Add role property in users model

--- src/models/user.ts
+++ src/models/user.ts
@@ -9,6 +9,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  role: {
+    type: String,
+    required: true
+  },
   orders: [
     {
       type: mongoose.Schema.Types.ObjectId,
